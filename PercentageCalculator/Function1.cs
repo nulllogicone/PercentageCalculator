a64fb84607c918594b15e151074a264d3cb2f777
Frederic Luchting <frederic@luchting.de>
2018-06-29 22:30:36 +0200

    Log requests and responses in Azure Table Storage

--- PercentageCalculator/Function1.cs
+++ PercentageCalculator/Function1.cs
@@ -1,23 +1,24 @@
 
-using System.IO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage.Table;
 using Newtonsoft.Json;
-using System.Text;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace PercentageCalculator
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static IActionResult Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, 
-            
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req,
+            [Table("Logs")]CloudTable percentageCalculationInputTable,
             TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
@@ -25,16 +26,27 @@ namespace PercentageCalculator
             string requestBody = new StreamReader(req.Body).ReadToEnd();
             var data = JsonConvert.DeserializeObject<Input>(requestBody);
             var sum = data.Values.Select(x => x.Value).Sum();
-            var result = new List<int>();
+            var percentages = new List<int>();
             foreach (var value in data.Values)
             {
                 var percentage = (int)System.Math.Round((decimal)value.Value / sum * 100);
-                result.Add(percentage);
+                percentages.Add(percentage);
             }
 
-            return result != null
-                ? (ActionResult)new OkObjectResult(result)
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+            var entity = new PercentageCalculatorEntity()
+            {
+                PartitionKey = System.Guid.NewGuid().ToString(),
+                RowKey = System.Guid.NewGuid().ToString(),
+                Request = requestBody,
+                Response = JsonConvert.SerializeObject(percentages)
+            };
+            var updateOperation = TableOperation.InsertOrReplace(entity);
+            await percentageCalculationInputTable.ExecuteAsync(updateOperation);
+
+            return (ActionResult)new OkObjectResult(percentages);
+            //return result != null
+            //    ? (ActionResult)new OkObjectResult(result)
+            //    : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
 }
---
+++ PercentageCalculator/PercentageCalculatorEntity.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace PercentageCalculator
+{
+    internal class PercentageCalculatorEntity :TableEntity
+    {
+        public PercentageCalculatorEntity()
+        {
+        }
+
+        public string Request { get; set; }
+        public string Response { get; set; }
+    }
+}
