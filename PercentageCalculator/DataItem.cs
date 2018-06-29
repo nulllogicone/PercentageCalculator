c88bd5f33834839c56e15b6e842d10889e735abc
Frederic Luchting <frederic@luchting.de>
2018-06-29 20:51:54 +0200

    Implement the percentage logic
    peter

---
+++ PercentageCalculator/DataItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PercentageCalculator
+{
+
+    public class Input
+    {
+        public List<DataItem> Values { get; set; }
+    }
+    public class DataItem
+    {
+        public string Label { get; set; }
+
+        public int Value { get; set; }
+    }
+}
--- PercentageCalculator/Function1.cs
+++ PercentageCalculator/Function1.cs
@@ -6,24 +6,34 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PercentageCalculator
 {
     public static class Function1
     {
         [FunctionName("Function1")]
-        public static IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, TraceWriter log)
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]HttpRequest req, 
+            
+            TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
             string requestBody = new StreamReader(req.Body).ReadToEnd();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            var data = JsonConvert.DeserializeObject<Input>(requestBody);
+            var sum = data.Values.Select(x => x.Value).Sum();
+            var result = new List<int>();
+            foreach (var value in data.Values)
+            {
+                var percentage = (int)System.Math.Round((decimal)value.Value / sum * 100);
+                result.Add(percentage);
+            }
 
-            return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+            return result != null
+                ? (ActionResult)new OkObjectResult(result)
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
